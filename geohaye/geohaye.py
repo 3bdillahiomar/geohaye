eba0221d079ccc5d5c8ddfee476d88fc602f3d87
Abdillahi Omar <zako3374@gmail.com>
2024-03-10 00:35:52 +0100

    Add base maps and layers control

--- geohaye/geohaye.py
+++ geohaye/geohaye.py
@@ -1,8 +1,24 @@
 """Main module."""
 
 import ipyleaflet
+from ipyleaflet import basemaps
 
 class Map(ipyleaflet.Map):
     def __init__(self, center=[9.56, 44.065], zoom=12, **kwargs):  # center=[lat, lon] # zoom=12 (default) Hargeisa Test
         super().__init__(center=center,zoom=zoom, **kwargs)
-        self.add_control(ipyleaflet.LayersControl())
+
+    def add_tile_layer(self, url, name, **kwargs):
+        layer = ipyleaflet.TileLayer(url=url, name=name, **kwargs)
+        self.add(layer)
+
+
+    def add_basemap(self, name):
+
+        if isinstance(name, str):
+            url = eval(f"basemaps.{name}").build_url()
+            self.add_tile_layer(url, name)
+        else:
+            self.add(name)
+
+    def add_layers_control(self, position='topright'):
+        self.add_control(ipyleaflet.LayersControl(position=position))
